07aef4cefcc40525dd75981c6230f57f1b40aebd
Sehajdeep Singh <sehajdeepsdsn@gmail.com>
2025-07-12 06:49:36 +0530

    Create script.js

---
+++ Submissions/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = new Typed("#typed-text", {
+    strings: [
+      "Data Science Enthusiast",
+      "Web Developer",
+      "AI Explorer",
+      "Hackathon Enthusiast",
+      "Creative Coder",
+      "Lifelong Learner"
+    ],
+    typeSpeed: 60,
+    backSpeed: 30,
+    loop: true
+  });
+});
